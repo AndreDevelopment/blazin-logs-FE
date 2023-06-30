59c6fcfa2b43df0543ad0873f738a8693b0ea024
Andre D'Souza <andredsouza15@gmail.com>
2023-06-30 10:20:53 -0400

    Created a NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,21 @@
+import "../index.css";
+import { BiRefresh } from "react-icons/bi";
+import { AiOutlineArrowUp } from "react-icons/ai";
+
 const NavBar = () => {
   return (
-    <div className="w-full py-8 px-4 bg-slate-800">
-      <div className="max-w-[1240px] mx-auto flex flex-row">
-        <h1 className="md:text-4xl 3xl">Blazin' Brawler Logs</h1>
-        <div>
-          <button>Refresh</button>
-          <button>Update</button>
+    <div className="w-full py-8 px-4 bg-black">
+      <div className=" mx-auto flex flex-row justify-between items-center">
+        <h1 className="md:text-4xl text-3xl font-['Carter_One',sans-serif]">
+          Blazin' Brawler Logs
+        </h1>
+        <div className="p-4 flex flex-row space-x-2">
+          <button className="p-2 border-solid border-2 border-green-700 rounded-md text-green-700 hover:bg-slate-700">
+            <BiRefresh />
+          </button>
+          <button className="p-2 border-solid border-2 border-red-700 rounded-md text-red-700 hover:bg-slate-700 ">
+            <AiOutlineArrowUp />
+          </button>
         </div>
       </div>
     </div>
